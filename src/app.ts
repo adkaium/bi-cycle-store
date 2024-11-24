7260123f060d388a6a8319d1744a72b6d4cd0a3d
Ad Kaium <adkaiumalikhan01@gmail.com>
2024-11-24 08:23:54 +0300

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application} from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response} from 'express';
+import cors from 'cors'
 import { ProductRoutes } from './app/modules/product/product.routes';
 import { OrderRoutes } from './app/modules/orders/order.router';
 const app: Application = express();
@@ -9,6 +9,9 @@ const app: Application = express();
 app.use(cors())
 app.use(express.json());
 
+app.get('/', (req:Request, res:Response)=>{
+     res.send('Hello')
+})
 
 //application routes
 app.use('/api/products', ProductRoutes);
