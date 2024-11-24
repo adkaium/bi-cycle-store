4bac6de935d43551ee1d82f994e33d05c569d6bb
Ad Kaium <adkaiumalikhan01@gmail.com>
2024-11-24 07:33:43 +0300

    add vercel

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application} from 'express';
+import cors from 'cors';
 import { ProductRoutes } from './app/modules/product/product.routes';
 import { OrderRoutes } from './app/modules/orders/order.router';
 const app: Application = express();
 
 
 // parsser
+app.use(cors())
 app.use(express.json());
 
 
